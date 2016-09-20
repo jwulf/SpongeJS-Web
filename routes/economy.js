b1f004fa54ca77cde33a59974c022c558469a701
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-09-20 12:13:16 -0400

    New route added

---
+++ routes/economy.js
@@ -0,0 +1,12 @@
+var conditional = require("conditional");
+var router = require('express').Router();
+
+router.get('/', function(req, res, next) {
+    
+});
+
+router.post('/sendMoney', function(req, res, next) {
+
+});
+
+module.exports = router;
