b2d1af0ba5c75855efa6cf1e0fdfdc7cf366584a
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-09-21 12:19:47 -0400

    Economy route

--- routes/economy.js
+++ routes/economy.js
@@ -2,7 +2,7 @@ var conditional = require("conditional");
 var router = require('express').Router();
 
 router.get('/', function(req, res, next) {
-    
+
 });
 
 router.post('/sendMoney', function(req, res, next) {
