f7a96862fc66b66ea1ed6d67fb40f660920e1ff2
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-09-22 17:56:20 -0400

    Login route added

---
+++ routes/login.js
@@ -0,0 +1,12 @@
+var conditional = require("conditional");
+var router = require('express').Router();
+
+router.get('/sendCode', function(req, res, next) {
+
+});
+
+router.get('/checkCode', function(req, res, next) {
+
+});
+
+module.exports = router;
