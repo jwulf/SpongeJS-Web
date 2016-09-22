9d140a315f412c4b3b88e31db60100b19244d41d
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-09-21 20:16:01 -0400

    public folder

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static('public'));
 
 app.use("/", require("./routes/views"));
 
